af795f13c7b550b7ab137563ed5eafa27eb0b150
varun <varunswarup0@gmail.com>
2020-04-10 18:27:49 +0530

    Reducer action & state

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 import id from 'uuid/v4';
 
@@ -7,22 +7,26 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
-const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+const reducer = (state, action) => {
+  return state;
+};
 
-  const addGrudge = grudge => {
+const Application = () => {
+  const [grudges, dispatch] = useReducer(reducer, initialState);
+
+  const addGrudge = (grudge) => {
     grudge.id = id();
     grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
+    // setGrudges([grudge, ...grudges]);
   };
 
-  const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
+  const toggleForgiveness = (id) => {
+    // setGrudges(
+    //   grudges.map((grudge) => {
+    //     if (grudge.id !== id) return grudge;
+    //     return { ...grudge, forgiven: !grudge.forgiven };
+    //   })
+    // );
   };
 
   return (
