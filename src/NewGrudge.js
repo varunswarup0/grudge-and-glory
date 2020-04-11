19202b71307ff48686728d60968838af0178e3a8
varun <varunswarup0@gmail.com>
2020-04-11 11:49:46 +0530

    Context Practice

--- src/NewGrudge.js
+++ src/NewGrudge.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import { GrudgeContext } from './GrudgeContext';
 
-const NewGrudge = React.memo(({ onSubmit }) => {
+const NewGrudge = React.memo(() => {
   const [person, setPerson] = useState('');
   const [reason, setReason] = useState('');
 
   console.log('Rendering New Grudge');
 
+  const { addGrudge } = React.useContext(GrudgeContext);
+
   const handleChange = (event) => {
     event.preventDefault();
-    onSubmit({ person, reason });
+    addGrudge({ person, reason });
   };
 
   return (
